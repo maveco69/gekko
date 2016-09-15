2f3d6746c13b7f7c02bef6871c71a7f7ae9735e4
andre.lima <andre.lima@softplan.com.br>
2016-09-15 18:05:00 -0300

    Early bid

---
+++ backtest.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+export COINS="BURST,BBR,CGA,HUC,DIEM,BITS,NOBL,QORA,GRC,C2,NSR,EMC2,VTC,XST,QBK,FLO,MYR,QTL,BELA,SDC,PINK,CURE,DGB,RIC,VIA,SBD,OMNI,XCN,VRC,LSK,VOX,1CR,XCP,BTM,NOTE,POT,SYNC,XVC,AMP,XDN,LBC,NBT,ETH,NXT,MAID,PPC,SJCX,LTC,BLK,XEM,STR,DOGE,SYS,BTS,XMR,ETC,IOC,MMNXT,XRP,DCR,NMC,BTCD,DASH,RADS,XPM,GAME,FCT,BITCNY,SC,NAUT,LTBC,NAV,GEO,STEEM,XBC,UNITY,CLAM,BCN,HZ,NEOS,EXP,BLOCK,RBY,XMG,RDD,FLDC,BCY"
+
+for coin in $(echo $COINS | sed "s/,/ /g")
+do
+    for day in {01..10}
+    do
+        declare day_end=$(printf "%02d" $((day+1)))
+        echo "$coin - $day - %coin - %day_start - %day_end" | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/$day_end/g"
+
+        cat cp-config.js | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/$day_end/g" > configs/$coin.config.js
+
+        node gekko.js --import --config configs/$coin.config.js
+        node gekko.js --backtest --config configs/$coin.config.js > logs/$coin.$day.log
+        #echo "2016-06-11 08:53:22 (INFO): (PROFIT REPORT) simulated yearly profit:   -435.53244 USD (-70.68152%)" > $i.log
+        egrep 'simulated profit' logs/$coin.$day.log >> profit.csv
+    done
+done
---
+++ configs/BURST.config.js
@@ -0,0 +1,111 @@
+﻿// Everything is explained here:
+// @link https://github.com/askmike/gekko/blob/stable/docs/Configuring_gekko.md
+
+var config = {};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                          GENERAL SETTINGS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.debug = true; // for additional logging / debugging
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                         WATCHING A MARKET
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Monitor the live market
+config.watch = {
+
+  // see https://github.com/askmike/gekko#supported-exchanges
+  exchange: 'Poloniex',
+  currency: 'USD',
+  asset: 'BURST'
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING TRADING ADVICE
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.tradingAdvisor = {
+  enabled: true,
+  method: 'candle',
+  candleSize: 60*12,
+  historySize: 10,
+  adapter: 'sqlite',
+  talib: {
+    enabled: false,
+    version: '1.0.2'
+  }
+};
+
+config.trader = {
+  enabled: false,
+  key: '',
+  secret: '',
+  username: '' // your username, only required for specific exchanges.
+};
+
+config.adviceLogger = {
+  enabled: true,
+  muteSoft: true // disable advice printout if it's soft
+};
+
+// do you want Gekko to calculate the profit of its own advice?
+config.profitSimulator = {
+  enabled: true,
+  // report the profit in the currency or the asset?
+  reportInCurrency: true,
+  // start balance, on what the current balance is compared with
+  simulationBalance: {
+    // these are in the unit types configured in the watcher.
+    asset: 1,
+    currency: 0.02,
+  },
+  // how much fee in % does each trade cost?
+  fee: 0.25,
+  // how much slippage/spread should Gekko assume per trade?
+  slippage: 0.05
+}
+
+config.candleWriter = {
+  adapter: 'sqlite',
+  enabled: true
+}
+
+config.adapters = {
+  sqlite: {
+    path: 'plugins/sqlite',
+
+    dataDirectory: './history',
+    version: 0.1,
+
+    dependencies: [{
+      module: 'sqlite3',
+      version: '3.1.4'
+    }]
+  }
+}
+
+config.backtest = {
+  adapter: 'sqlite',
+  daterange: {
+    from: "2016-09-03 12:00:00",
+    to: "2016-09-04 12:00:00"
+  },
+  batchSize: 50
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING IMPORTING
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.importer = {
+  daterange: {
+    from: "2016-09-03 12:00:00",
+    to: "2016-09-04 12:00:00"
+  }
+}
+
+config['I understand that Gekko only automates MY OWN trading strategies'] = false;
+
+module.exports = config;
