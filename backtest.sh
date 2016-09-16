5b0476a840543c218e0938de0edd8eaf38ab64aa
andre.lima <andre.lima@softplan.com.br>
2016-09-16 16:44:40 -0300

    Removed some...

--- backtest.sh
+++ backtest.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-export COINS="BURST,BBR,CGA,HUC,DIEM,BITS,NOBL,QORA,GRC,C2,NSR,EMC2,VTC,XST,QBK,FLO,MYR,QTL,BELA,SDC,PINK,CURE,DGB,RIC,VIA,SBD,OMNI,XCN,VRC,LSK,VOX,1CR,XCP,BTM,NOTE,POT,SYNC,XVC,AMP,XDN,LBC,NBT,ETH,NXT,MAID,PPC,SJCX,LTC,BLK,XEM,STR,DOGE,SYS,BTS,XMR,ETC,IOC,MMNXT,XRP,DCR,NMC,BTCD,DASH,RADS,XPM,GAME,FCT,BITCNY,SC,NAUT,LTBC,NAV,GEO,STEEM,XBC,UNITY,CLAM,BCN,HZ,NEOS,EXP,BLOCK,RBY,XMG,RDD,FLDC,BCY"
+#BURST,BBR,CGA,HUC,DIEM,BITS,NOBL,QORA,GRC,C2,NSR,EMC2,VTC,XST,QBK,FLO,MYR,QTL,BELA,SDC,PINK,CURE,DGB,RIC,VIA,SBD,OMNI,XCN,
+export COINS="VRC,LSK,VOX,1CR,XCP,BTM,NOTE,POT,SYNC,XVC,AMP,XDN,LBC,NBT,ETH,NXT,MAID,PPC,SJCX,LTC,BLK,XEM,STR,DOGE,SYS,BTS,XMR,ETC,IOC,MMNXT,XRP,DCR,NMC,BTCD,DASH,RADS,XPM,GAME,FCT,BITCNY,SC,NAUT,LTBC,NAV,GEO,STEEM,XBC,UNITY,CLAM,BCN,HZ,NEOS,EXP,BLOCK,RBY,XMG,RDD,FLDC,BCY"
 
 for coin in $(echo $COINS | sed "s/,/ /g")
 do
-    for day in {01..01}
+    for day in {01..14}
     do
         declare day_end=$(printf "%02d" $((day+1)))
         echo "$coin - $day - %coin - %day_start - %day_end" | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/$day_end/g"
 
-        cat cp-config.js | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/15/g" > configs/$coin.config.js
+        cat cp-config.js | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/$day_end/g" > configs/$coin.config.js
 
         node --max_old_space_size=4096 gekko.js --import --config configs/$coin.config.js
         #node gekko.js --backtest --config configs/$coin.config.js > logs/$coin.$day.log
