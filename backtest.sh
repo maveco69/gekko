8e4f1a99736284a0652c503a1d290de412d23c5c
andre.lima <andre.lima@softplan.com.br>
2016-09-19 17:28:49 -0300

    new period again

--- backtest.sh
+++ backtest.sh
@@ -5,15 +5,15 @@ declare full_profit=0.0
 for coin in $(echo $COINS | sed "s/,/ /g")
 do
     declare coin_profit=0.0
-    for day in {15..18}
+    for day in {01..18}
     do
         declare day_end=$(printf "%02d" $((day+1)))
         echo "$coin - $day - %coin - %day_start - %day_end" | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/$day_end/g"
 
         cat cp-config.js | sed -e "s/%coin/$coin/g; s/%year/2016/g; s/%month/09/g; s/%day_start/$day/g; s/%day_end/$day_end/g" > configs/$coin.config.js
 
-        node gekko.js --import --config configs/$coin.config.js
-        #node gekko.js --backtest --config configs/$coin.config.js > logs/$coin.$day.log
+        #node gekko.js --import --config configs/$coin.config.js
+        node gekko.js --backtest --config configs/$coin.config.js > logs/$coin.$day.log
         #echo "2016-06-11 08:53:22 (INFO): (PROFIT REPORT) simulated yearly profit:   -435.53244 USD (-70.68152%)" > $i.log
         profit=$(egrep 'simulated profit' logs/$coin.$day.log | sed "s/.*:.*:.*BTC (//g; s/%)//g; s/[[:blank:]]//g;")
         coin_profit=$(awk '{print $1+$2}' <<<"$profit $coin_profit")
